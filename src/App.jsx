a9100ec85e697ac38ead26ce1cdd9999c8ceb71e
Xun Zhu <xzhu1@stjude.org>
2023-08-29 14:03:23 +0000

    fixup! fixup! fixup! fixup! Init

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,15 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import ClinicalReport from "./components/ClinicalReport";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div className='m-4'>
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-          onClick={() => setCount((count) => count + 1)}>
-          START HERE {count}
-        </button>
-      </div>
+      <ClinicalReport />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/ClinicalReport.jsx
@@ -0,0 +1,10 @@
+function ClinicalReport({}) {
+  return (
+    <div className="mx-auto max-w-2xl flex flex-col gap-2">
+      <div className="text-lg font-bold">Clinical Report</div>
+      <div className="h-96 border rounded p-2">START HERE</div>
+    </div>
+  );
+}
+
+export default ClinicalReport;
